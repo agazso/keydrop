f00d99deaf1d2f53db78ea4306d9ddcdcb499453
Attila Gazso <agazso@gmail.com>
2018-04-23 20:32:41 +0200

    Added default state, HomeScreen, ContactList

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
 import * as React from 'react';
-import {
-    createStore,
-    combineReducers,
-    applyMiddleware,
-    compose,
-} from 'redux';
-import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { StackNavigator, TabNavigator, NavigationRouteConfigMap } from 'react-navigation';
 import { View, Text, StatusBar, Platform } from 'react-native';
 
-interface AppState {
-}
-
-export const appStateReducer = (state: AppState = {}, action: any): AppState => {
-    return state;
-};
-
-const defaultState: AppState = {};
-const reducer = combineReducers<AppState>({appStateReducer});
-const store = createStore(
-    reducer,
-    defaultState,
-    compose(
-        applyMiddleware(thunkMiddleware)
-    )
-);
-store.subscribe(() => console.log(store.getState()));
-
-const HomeScreen = (props) => <View><Text>Hello</Text></View>;
+import { store } from './reducers/index';
+import { HomeScreenContainer } from './containers/HomeScreenContainer';
 
 const AppNavigator = StackNavigator(
     {
-        HomeScreen: { screen: HomeScreen},
+        HomeScreen: { screen: HomeScreenContainer},
+    },
+    {
+        navigationOptions: {
+            headerTitle: <Text>Keydrop</Text>,
+        },
     }
 );
 
