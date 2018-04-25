c0cc8decf5300a61b4ee1bbbeedde274e4e1ea76
Attila Gazso <agazso@gmail.com>
2018-04-25 17:42:36 +0200

    Added filtering for online contacts

--- src/containers/HomeScreenContainer.ts
+++ src/containers/HomeScreenContainer.ts
@@ -7,8 +7,15 @@ import { HomeScreen, StateProps, DispatchProps } from '../components/HomeScreen'
 import * as Actions from '../actions/Actions';
 
 const mapStateToProps = (state: AppState, ownProps): StateProps => {
+    const OnlineTimestampMillis = 60 * 1000;
+    const contacts = state.contacts.toArray();
+    const onlineContacts = contacts.filter(contact =>
+        contact.lastSeen > state.currentTimestamp - OnlineTimestampMillis);
+
+    console.log(onlineContacts);
+
     return {
-        contacts: state.contacts.toArray(),
+        contacts: onlineContacts,
         alreadyHasKey: state.user.name !== '',
         user: state.user,
     };
