e0ee95da552d423690cba8f1700c05f55bd7d3ff
Attila Gazso <agazso@gmail.com>
2018-04-25 17:43:23 +0200

    Added placeholder graphics & animation when nobody is online, animations are disabled right now

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -9,6 +9,7 @@ import {
     Button,
     TouchableWithoutFeedback,
     Alert,
+    Easing,
 } from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -51,32 +52,98 @@ type ContactListProps = ContactListStateProps & ContactListDispatchProps;
 
 export class ContactList extends React.PureComponent<ContactListProps> {
     public render() {
-        return (
-            <AnimatedList
-                ListFooterComponent={<View style={{paddingBottom: PaddingBottom}} ></View>}
-                ListHeaderComponent={
-                    <ListHeader
-                        user={this.props.user}
-                        onCreateContact={this.props.onCreateContact}
-                    />
-                }
-                renderItem={
-                    (item) => (
-                        <ContactItem
-                            item={item.item}
+        if (this.props.contacts.length === 0) {
+            return (
+                <EmptyListPlaceholder
+                    ListHeaderComponent={
+                        <ListHeader
+                            user={this.props.user}
+                            onCreateContact={this.props.onCreateContact}
                         />
-                    )
-                }
-                keyExtractor={this.keyExtractor}
-                keyboardDismissMode='interactive'
-                maxToRenderPerBatch={15}
-                data={this.props.contacts}
-            />
-        );
+                    }
+                />
+            );
+        } else {
+            return <this.List/>;
+        }
     }
+
+    private List = (props) => (
+        <AnimatedList
+            ListFooterComponent={<View style={{paddingBottom: PaddingBottom}} ></View>}
+            ListHeaderComponent={
+                <ListHeader
+                    user={this.props.user}
+                    onCreateContact={this.props.onCreateContact}
+                />
+            }
+            renderItem={
+                (item) => (
+                    <ContactItem
+                        item={item.item}
+                    />
+                )
+            }
+            keyExtractor={this.keyExtractor}
+            keyboardDismissMode='interactive'
+            maxToRenderPerBatch={15}
+            data={this.props.contacts}
+        />
+    )
 
     private keyExtractor = (contact: Contact, index: number) => {
         return '' + contact._id;
+    }
+}
+
+interface EmptyListPlaceholderProps {
+    ListHeaderComponent: JSX.Element;
+}
+
+class EmptyListPlaceholder extends React.PureComponent<EmptyListPlaceholderProps> {
+    // Disabled animation, because it causes the backboardd process
+    // to use more than 100% CPU, to be investigated
+    private isAnimated = false;
+    private animatedValue = new Animated.Value(1);
+
+    public componentDidMount() {
+        if (this.isAnimated) {
+            this.animate();
+        }
+    }
+
+    public render() {
+        const opacity = this.isAnimated
+            ? this.animatedValue.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [0, 1, 0],
+              })
+            : 1
+            ;
+
+        return (
+            <View style={styles.placeholderContainer}>
+                {this.props.ListHeaderComponent}
+                <View style={styles.wavePlaceholderContainer}>
+                    <Animated.View style={[styles.placeholderWawe1, {opacity}]} />
+                    <Animated.View style={[styles.placeholderWawe2, {opacity}]} />
+                    <Animated.View style={[styles.placeholderWawe3, {opacity}]} />
+                    <Animated.View style={[styles.placeholderWawe4, {opacity}]} />
+                    <Animated.View style={[styles.placeholderWawe5, {opacity}]} />
+                </View>
+            </View>
+        );
+    }
+
+    private animate() {
+        this.animatedValue.setValue(0);
+        Animated.timing(
+            this.animatedValue, {
+                toValue: 1,
+                duration: 3000,
+                easing: Easing.linear,
+            },
+        ).start(() => this.animate());
     }
 }
 
@@ -228,6 +295,14 @@ class ListHeader extends React.PureComponent<ListHeaderProps, ListHeaderState> {
     }
 }
 
+const Wave1Size = 200;
+const Wave2Size = 320;
+const Wave3Size = 440;
+const Wave4Size = 560;
+const Wave5Size = 680;
+const WaveColor = Colors.LIGHT_GRAY;
+const WaveWidth = 0.8;
+
 const styles = StyleSheet.create({
     listItem: {
         flexDirection: 'column',
@@ -250,6 +325,7 @@ const styles = StyleSheet.create({
         height: 200,
         flexDirection: 'column',
         justifyContent: 'space-between',
+        backgroundColor: Colors.WHITE,
     },
     listHeaderButtonContainer: {
         flexDirection: 'row',
@@ -280,5 +356,65 @@ const styles = StyleSheet.create({
     qrCodeContainer: {
         width: QRCodeWidth,
         height: QRCodeHeight,
+    },
+    placeholderContainer: {
+        backgroundColor: Colors.WHITE,
+        padding: 0,
+        margin: 0,
+        height: '100%',
+    },
+    wavePlaceholderContainer: {
+        height: '100%',
+        overflow: 'hidden',
+    },
+    placeholderWawe1: {
+        width: Wave1Size,
+        height: Wave1Size,
+        borderColor: WaveColor,
+        borderRadius: Wave1Size,
+        borderWidth: WaveWidth,
+        position: 'absolute',
+        bottom: 0,
+        left: Width / 2 - Wave1Size / 2,
+    },
+    placeholderWawe2: {
+        width: Wave2Size,
+        height: Wave2Size,
+        borderColor: WaveColor,
+        borderRadius: Wave2Size,
+        borderWidth: WaveWidth,
+        position: 'absolute',
+        bottom: 0,
+        left: Width / 2 - Wave2Size / 2,
+    },
+    placeholderWawe3: {
+        width: Wave3Size,
+        height: Wave3Size,
+        borderColor: WaveColor,
+        borderRadius: Wave3Size,
+        borderWidth: WaveWidth,
+        position: 'absolute',
+        bottom: 0,
+        left: Width / 2 - Wave3Size / 2,
+    },
+    placeholderWawe4: {
+        width: Wave4Size,
+        height: Wave4Size,
+        borderColor: WaveColor,
+        borderRadius: Wave4Size,
+        borderWidth: WaveWidth,
+        position: 'absolute',
+        bottom: 0,
+        left: Width / 2 - Wave4Size / 2,
+    },
+    placeholderWawe5: {
+        width: Wave5Size,
+        height: Wave5Size,
+        borderColor: WaveColor,
+        borderRadius: Wave5Size,
+        borderWidth: WaveWidth,
+        position: 'absolute',
+        bottom: 0,
+        left: Width / 2 - Wave5Size / 2,
     },
 });
