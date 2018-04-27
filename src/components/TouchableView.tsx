7fb6daaaa001f2c83c8d0806b13dde7cc4cd5252
Attila Gazso <agazso@gmail.com>
2018-04-27 15:04:01 +0200

    Added default hitSlop to TouchableView

--- src/components/TouchableView.tsx
+++ src/components/TouchableView.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { View, Text, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
 
+const defaultHitSlop = {
+    top: 15,
+    left: 15,
+    bottom: 15,
+    right: 15,
+};
+
 export const TouchableView = (props) => (
-    <TouchableWithoutFeedback onPress={props.onPress}>
+    <TouchableWithoutFeedback onPress={props.onPress} hitSlop={props.hitSlop ? props.hitSlop : defaultHitSlop}>
         <View {...props} >
             {props.children}
         </View>
