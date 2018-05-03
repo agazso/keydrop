af882b5cc63c68638ab9beea24593f4664df66f8
Attila Gazso <agazso@gmail.com>
2018-05-03 16:02:13 +0200

    Added tests

---
+++ test/ContactTest.ts
@@ -0,0 +1,29 @@
+import { Contact, isContactPersistent } from '../src/models/Contact';
+
+it('should test if contact is persistent', () => {
+    const contact: Contact = {
+        type: 'person',
+        state: 'contact',
+        name: 'Alice',
+        publicKey: '0xPUBKEY',
+        knownSince: 0,
+        lastSeen: 0,
+    };
+
+    const result = isContactPersistent(contact);
+    expect(result).toBeTruthy();
+});
+
+it('should test if contact is not persistent', () => {
+    const contact: Contact = {
+        type: 'person',
+        state: 'invite-sent',
+        name: 'Alice',
+        publicKey: '0xPUBKEY',
+        knownSince: 0,
+        lastSeen: 0,
+    };
+
+    const result = isContactPersistent(contact);
+    expect(result).toBeFalsy();
+});
