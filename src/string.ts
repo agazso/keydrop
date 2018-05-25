04772a6d8b72d7926e858f5eeed8db625fb4450d
Attila Gazso <agazso@gmail.com>
2018-05-25 19:07:32 +0200

    Added string utilities

---
+++ src/string.ts
@@ -0,0 +1,26 @@
+export const hexToString = (hex: string): string => {
+    const hexWithoutPrefix = hex.startsWith('0x') ? hex.slice(2) : hex;
+    const subStrings: string[] = [];
+    for (let i = 0; i < hexWithoutPrefix.length; i += 2) {
+        subStrings.push(hexWithoutPrefix.substr(i, 2));
+    }
+    const numbers = subStrings.map(s => parseInt(s, 16));
+    return String.fromCodePoint(...numbers);
+};
+
+// cheekily borrowed from https://stackoverflow.com/questions/34309988/byte-array-to-hex-string-conversion-in-javascript
+export const toHexString = (byteArray: Array<number>) => {
+    return Array.from(byteArray, (byte) => {
+        // tslint:disable-next-line:no-bitwise
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+};
+
+// equally cheekily borrowed from https://stackoverflow.com/questions/17720394/javascript-string-to-byte-to-string
+export const string2Bin = (str: string) => {
+    const result = new Array<number>();
+    for (let i = 0; i < str.length; i++) {
+        result.push(str.charCodeAt(i));
+    }
+    return result;
+};
